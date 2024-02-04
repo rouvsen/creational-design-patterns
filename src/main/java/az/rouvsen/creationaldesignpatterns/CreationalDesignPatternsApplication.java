0ec06ca224c01b3bdf8640c702649aaa6bb830f3
rouvsen <rouvsen@gmail.com>
2024-02-04 20:43:22 +0400

    initial commit

---
+++ src/main/java/az/rouvsen/creationaldesignpatterns/CreationalDesignPatternsApplication.java
@@ -0,0 +1,13 @@
+package az.rouvsen.creationaldesignpatterns;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CreationalDesignPatternsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CreationalDesignPatternsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/az/rouvsen/creationaldesignpatterns/CreationalDesignPatternsApplicationTests.java
@@ -0,0 +1,13 @@
+package az.rouvsen.creationaldesignpatterns;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CreationalDesignPatternsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
